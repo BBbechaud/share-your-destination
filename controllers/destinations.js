f16f5a7823a14da5b27416383e29384ab7818e5c
BBbechaud <132162306+BBbechaud@users.noreply.github.com>
2024-01-02 17:41:50 -0500

    Style changes to feed and profile pages

--- controllers/destinations.js
+++ controllers/destinations.js
@@ -8,7 +8,7 @@ module.exports = {
       //Since we have a session each request (req) contains the logged-in users info: req.user
       //console.log(req.user) to see everything
       //Grabbing just the posts of the logged-in user
-      const destination = await Destination.find({ user: req.user.id });
+      const destination = await Destination.find({ user: req.user.id }).sort({ createdAt: "desc" });
       //Sending post data from mongodb and user data to ejs template
       res.render("profile.ejs", { destination: destination, user: req.user });
     } catch (err) {
